028c27e501dff416152ee73ea137f0f6d8fc7094
chengcheng <chengcheng.frontend@bytedance.com>
2018-03-20 10:20:16 +0800

    difficulty revise

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ export const PLANE_HEIGHT = 40
 
 export const MISSILE_WIDTH = 5
 export const MISSILE_HEIGHT = 5
-export const INIT_MISSILE_SPEED = 1.4
+export const INIT_MISSILE_SPEED = 2.1
 export const INIT_MISSILE_POSITION_SEED = 3.8 // 开始位置，必须为大于等于3的数字，如果大于3, 则子弹可能从中间出现
 export const MISSILE_ENLARGE_MAX_FACTOR = 1.7 // 长宽放大的比例
 export const MISSILE_TAILING_FACTOR = 2 // 跟随飞机的系数，如果设置为0，表示子弹的行为和飞机的位置没有关系
